e22bb60a354fc799cff7ab6867902dd65ef10e0c
Yuraga1 <32905480+Yuraga1@users.noreply.github.com>
2017-11-16 00:54:56 +0700

    Add files via upload

--- TGR.py
+++ TGR.py
@@ -1,4 +1,10 @@
 from appJar import*
+z=''
+f = open('History.txt')
+for line in f:
+    line
+    print(line)
+    z=z+line
 
 def press(button):
 	if button == 'Начать работу':
@@ -14,7 +20,10 @@ app=gui("test","350x400")
 #app.setBg('red')
 
 app.startSubWindow("База знаний", modal=True)
-app.addLabel("l2", "База знаний")
+#app.addLabel("l2", "База знаний")
+app.addMessage('Box1', z)
+app.startScrollPane('z')
+app.stopScrollPane()
 app.stopSubWindow()
 
 
