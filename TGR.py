2f9a5208d37b439ada8f1165f342c5b170cca9c5
Yuraga1 <32905480+Yuraga1@users.noreply.github.com>
2017-11-15 21:06:39 +0700

    Add files via upload

--- TGR.py
+++ TGR.py
@@ -1,9 +1,31 @@
-from appJar import gui
+from appJar import*
 
-app=gui('test','800x600')
+def press(button):
+	if button == 'Начать работу':
+		print('Начать работу')
+	else:
+		app.showSubWindow(button)
+		#print('База знаний')
+
+def knop(btv):
+	print('Zvezda')
+
+app=gui("test","350x400")
 #app.setBg('red')
 
-app.addLabel('l1','Hello World')
+app.startSubWindow("База знаний", modal=True)
+app.addLabel("l2", "База знаний")
+app.stopSubWindow()
+
+
+app.addLabel("l1","Разделы")
+app.addCheckBox("Всемирная история")
+app.addCheckBox("История России")
+
+app.addLabelNumericEntry("Колличество вопросов")
+
+app.addButtons(["Начать работу", "База знаний"], press)
+app.addImageButton('Knopka', knop, 'book2.gif')
 
 
 app.go()
